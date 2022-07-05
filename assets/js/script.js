0813fa815c929d574cf9bf666a4c790cd73d2e6d
Ubirajara Neves <ubirajara.neves@usp.br>
2022-07-05 11:59:46 -0300

    Code commented

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Use of a regular expression to format a number with Brazilian rules.
 function formatNumber(num) {
 
     return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1&nbsp;');
@@ -6,19 +7,22 @@ function formatNumber(num) {
 
 async function fetchCovidData() {
 
+    // Init configuration for fetch function. Here, we assure the use of method GET to call the API.
     const fetchInit = {
         method: 'GET',
     };
 
+    // The API with Covid-19 data for the state of Sao Paulo, Brazil
     const res = await fetch("https://covid19-brazil-api.vercel.app/api/report/v1/brazil/uf/sp", fetchInit);
     const rec = await res.json();
 
+    // Preparing date to be showed
     let date = new Date(rec.datetime)
-
     let day = (date.getDate() < 10 ? "0" + date.getDate() : date.getDate());
     let month = ((date.getMonth() + 1) < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1);
     let year = date.getFullYear();
 
+    // Data presentation
     document.getElementById("date").innerHTML = day + "/" + month + "/" + year;
     document.getElementById("cases").innerHTML = formatNumber(rec.cases);
     document.getElementById("deaths").innerHTML = formatNumber(rec.deaths);
